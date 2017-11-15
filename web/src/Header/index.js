6ae25a13567dd4934a804f752d38a2e89b632c18
Your Name <bora0072@umn.edu>
2017-11-15 01:19:12 -0600

    Changed Opacity

--- web/src/Header/index.js
+++ web/src/Header/index.js
@@ -11,7 +11,7 @@ class Header extends Component {
   render() {
     return (
       <div>
-      <nav className="navbar is-link" aria-label="main navigation">
+      <nav className="navbar is-dark" aria-label="main navigation">
         <div className="navbar-brand">
           <a class="navbar-item title has-text-success" href="/">Reacty  </a>
         </div>
